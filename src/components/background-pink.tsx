e443afdb96ac34b5f21571000ca17e81c24d1f02
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-07-13 00:44:39 -0300

    style: mudança de estilo

--- src/components/background-pink.tsx
+++ src/components/background-pink.tsx
@@ -1,5 +1,5 @@
 export default function BackgroundPink() {
   return (
-    <div className="absolute right-[50%] top-0  h-[20%] md:h-[30%]  w-full -translate-y-1/2 translate-x-1/2 overflow-hidden rounded-full bg-secondarypink px-28 py-16 opacity-50 blur-full" />
+    <div className="absolute right-[50%] top-0  h-[10%] md:h-[10%]  w-full -translate-y-1/2 translate-x-1/2 overflow-hidden rounded-full bg-secondarypink px-28 py-16 opacity-50 blur-full" />
   )
 }
