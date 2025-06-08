4695aa2cea1e9a3365eb10c50b80010eab5e066d
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-06-08 00:39:07 -0300

    :pencil: alteração de link

--- src/data/filtroData.ts
+++ src/data/filtroData.ts
@@ -5,7 +5,7 @@ export const FiltroData: FiltroProps[] = [
     id: 1,
     title: 'Moldura Sua Vez',
     subtitles: ['Moldura Sua Vez no tema da festa + plaquinha com QR code.'],
-    button: 'https://www.instagram.com/ar/834731952089590/',
+    button: 'https://www.instagram.com/elisaartes.molduras',
     info: 'Imagem ilustrativa. Criamos qualquer tema.',
     img: ['/suavez.webp', '/qrcodesuavez.webp'],
     price: 'R$ 35,00 ',
